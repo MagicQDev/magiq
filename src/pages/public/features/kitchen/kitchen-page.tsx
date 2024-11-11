f854928d11e5537a2662421a2d009bd98a6d7a4c
Emer Arteaga <arteagaemer@gmail.com>
2024-11-11 11:21:15 -0500

    inicio de componente cocina

--- src/pages/public/features/kitchen/kitchen-page.tsx
+++ src/pages/public/features/kitchen/kitchen-page.tsx
@@ -1,5 +1,21 @@
+import SubTitle from "@/components/app/subtitle";
+import Title from "@/components/app/title";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
 function KitchenPage() {
-  return <div>Kitchen</div>;
+  return (
+    <div className="w-full">
+      <Title>Mi cocina</Title>
+      <SubTitle>
+        Administra, crea y edita los pedidos de tu restaurante
+      </SubTitle>
+      <Separator></Separator>
+      <div className="flex w-full py-2 justify-end md:justify-around">
+        <Button>Nuevo pedido</Button>
+      </div>
+    </div>
+  );
 }
 
 export default KitchenPage;
